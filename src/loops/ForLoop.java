f8a1dd4933b44684c6a9919f04e99d649fabb2b8
Happy <biswasnedhi09@gmail.com>
2023-09-01 13:27:01 -0500

    Added Loops File

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args)
+    {
+        for(int b = 1; b <= 30; b+=2) //print the odd number
+        {
+            System.out.println(b);
+        }
+
+        for(int i = 2; i <= 30; i+=2) //print the even number
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/loops/WhileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class WhileLoop {
+    public static void main(String[] args)
+    {
+        int a = 1;
+        while(a <= 7)
+        {
+            System.out.println("Hello");
+            a++;
+        }
+    }
+}
