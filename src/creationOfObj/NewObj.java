e0f255d0b4a006b10268531e9b0b93d63e8e0728
Happy <biswasnedhi09@gmail.com>
2023-09-03 14:42:54 -0500

    started java object

---
+++ src/creationOfObj/NewObj.java
@@ -0,0 +1,17 @@
+package creationOfObj;
+
+public class NewObj {
+    public static void main(String[] args) {
+        //ClassName ref = new Constructor();
+        NewObj obj = new NewObj();
+        obj.method1();
+
+    }
+
+    void method1()
+    {
+        System.out.println("This is method 1");
+    }
+}
+
+
