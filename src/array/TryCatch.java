77fc2c1d7df3d8c7ed4225c0b628c761ea37bd47
Happy <biswasnedhi09@gmail.com>
2023-09-13 22:46:10 -0500

    updated try-catch

--- src/array/TryCatch.java
+++ src/array/TryCatch.java
@@ -1,6 +1,7 @@
 package array;
 
 public class TryCatch {
+
     public static void main(String[] args) {
         char[] x = {'a', 'b', 'c', 'd', 'e'};
 
@@ -13,5 +14,27 @@ public class TryCatch {
             System.out.println("catch the exception");
         }
         System.out.println(x[2]);
+        // Locally / inside the main method we catch the exception by try-catch block
+
+        System.out.println("First");
+        try{
+            Thread.sleep(10000);
+        }catch(Exception m)
+        {
+
+        }
+        System.out.println("second");
     }
+
+
+    /*
+    we can do both way to throw the exception
+    // with the main method / globally throw the exception
+    
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("first");
+        Thread.sleep(10000);
+        System.out.println("second");
+
+    }*/
 }
